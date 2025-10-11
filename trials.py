d595474498ee53ff5e56b0f1978555287e81f3ad
Jessie Guo <jessie.guo2103@gmail.com>
2025-10-11 23:00:58 +1100

    added plots, tried running - failed

--- trials.py
+++ trials.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import MinMaxScaler
 from sklearn import datasets, linear_model
 from sklearn.neural_network import MLPClassifier
 from sklearn.neural_network import MLPRegressor
@@ -13,20 +13,14 @@ import pandas as pd
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 
-NUM_EXPERIMENTS = 30
-
 def split_data(i, data_inputx, data_inputy):
 
     x_train, x_test, y_train, y_test = train_test_split(data_inputx, data_inputy, test_size=0.4, random_state=i)
 
     return x_train, x_test, y_train, y_test
 
-def neural(x_train, x_test, y_train, y_test, neurons, layers, learning): #compare with best approach from prev qu.
-    transformer = Normalizer().fit(x_train.iloc[:,1:])
-    x_train.iloc[:, 1:] = transformer.transform(x_train.iloc[:, 1:])
-    x_test.iloc[:, 1:] = transformer.transform(x_test.iloc[:, 1:])
-  
-    mlp = MLPRegressor(hidden_layer_sizes=(neurons, ), activation='identity', solver='sgd', learning_rate_init = learning, max_iter=1000)
+def neural(x_train, x_test, y_train, y_test, layers, learning): #compare with best approach from prev qu.
+    mlp = MLPRegressor(hidden_layer_sizes=layers, activation='identity', solver='sgd', learning_rate_init = learning, max_iter=1000)
     mlp.fit(x_train, y_train)
 
     y_pred = mlp.predict(x_test)
@@ -38,36 +32,56 @@ def neural(x_train, x_test, y_train, y_test, neurons, layers, learning): #compar
 def process_data(df):
     df.iloc[:, 0] = df.iloc[:, 0].replace({"M": 0, "F": 1, "I": 2})
 
-
 def main():
     df = pd.read_csv('abalone.data', header=None, names=["sex", "length", "diameter", "height", "whole_weight", "shucked_weight", "viscera_weight", "shell_weight", "rings"])
     process_data(df)
     # gen_plots(df)
-    print(df)
-
-    rmse_nn = np.zeros(NUM_EXPERIMENTS)
-    rsquared_nn = np.zeros(NUM_EXPERIMENTS)
 
     data_inputx = df.iloc[:, 0:-1]
     data_inputy = df.iloc[:, -1]
-    print(data_inputx)
-    print(data_inputy)
 
-    learning = range(0.01, 0.1, 0.01)
-    layers = range()
-    neurons = range(3, 9, 1)
+    x_train, x_test, y_train, y_test = split_data(1, data_inputx, data_inputy)
 
-    for i in range(learning):
-        for j in range(layers):
-            for k in range(neurons):
-                rmse_val, rsquared_val = neural(x_train, x_test, y_train, y_test, k, j, i)
-                rmse_nn[i], rsquared_nn[i] = rmse_val, rsquared_val
+    transformer = MinMaxScaler().fit(x_train.iloc[:,1:])
+    x_train.iloc[:, 1:] = transformer.transform(x_train.iloc[:, 1:])
+    x_test.iloc[:, 1:] = transformer.transform(x_test.iloc[:, 1:])
+  
+    learning = np.arange(0.01, 0.1, 0.01)
 
-    print(f"RMSE of Neural Network Mean: {rmse_nn.mean()}")
-    print(f"RMSE of Neural Network SD: {rmse_nn.sd()}")
-    print(f"Rsquared of Neural Network Mean: {rsquared_nn.mean()}")
-    print(f"Rsquared of Neural Network SD: {rsquared_nn.sd()}")
+    layers = [(10, ), (20, ), (30, ), 
+          (10, 10), (10, 20), (10, 30),
+          (10, 10), (20, 10), (30, 10),
+          (10, 10, 10), (10, 10, 10, 10), (10, 10, 10, 10, 10)]
+    
+    print(df.describe().T)
 
-#Ask about reporting for training set
+    for j in layers:
+        rmse_val = []
+        for i in learning:
+            rmse, rsquared = neural(x_train, x_test, y_train, y_test, j, i)
+            rmse_val = np.append(rmse_val, rmse)
+       
+        plt.plot(learning, rmse_val, linestyle='--', label=f'{j}')
+        
+    plt.xlabel('learning rate')
+    plt.ylabel('RMSE')
+    plt.legend()
+    plt.savefig('plot.png')
+    plt.close()
+   
+    for j in layers:
+          rsquared_val = []
+            for i in learning:
+                rmse, rsquared = neural(x_train, x_test, y_train, y_test, j, i)
+                rsquared_val = np.append(rsquared_val, rsquared)
+        
+            plt.plot(learning, rsquared_val, linestyle='--', label=f'{j}')
+            
+    plt.xlabel('learning rate')
+    plt.ylabel('R^2')
+    plt.legend()
+    plt.savefig('plot.png')
+    plt.close()  
+    
 if __name__ =='__main__':
     main() 
