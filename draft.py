29c97064ece74cd15d25bf3f2f73d6f598d55d8a
Jessie Guo <jessie.guo2103@gmail.com>
2025-10-11 23:24:33 +1100

    added training through two selected features with corresponding metrics, changed from normalizer to minmaxscaler as per tip.

--- draft.py
+++ draft.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plt
-from sklearn.preprocessing import Normalizer
+from sklearn.preprocessing import MinMaxScaler
 from sklearn import datasets, linear_model
 from sklearn.neural_network import MLPClassifier
 from sklearn.neural_network import MLPRegressor
@@ -64,7 +64,7 @@ def scipy_linear_mod(x_train_real, x_test_real, y_train, y_test, transform, mode
     if transform == True:
         x_train = x_train_real.copy()
         x_test = x_test_real.copy()
-        transformer = Normalizer().fit(x_train.iloc[:,1:])
+        transformer = MinMaxScaler().fit(x_train.iloc[:,1:])
         x_train.iloc[:, 1:] = transformer.transform(x_train.iloc[:, 1:])
         x_test.iloc[:, 1:] = transformer.transform(x_test.iloc[:, 1:])
     else:
@@ -132,8 +132,80 @@ def scipy_linear_mod(x_train_real, x_test_real, y_train, y_test, transform, mode
 
         return acc, auc
 
-def neural(x_train, x_test, y_train, y_test): #compare with best approach from prev qu.
-    transformer = Normalizer().fit(x_train.iloc[:,1:])
+def selected(x_train_real, x_test_real, y_train, y_test, transform, model):
+    if transform == True:
+        x_train = x_train_real.copy()
+        x_test = x_test_real.copy()
+        transformer = MinMaxScaler().fit(x_train.iloc[:,1:])
+        x_train.iloc[:, 1:] = transformer.transform(x_train.iloc[:, 1:])
+        x_test.iloc[:, 1:] = transformer.transform(x_test.iloc[:, 1:])
+    else:
+        x_train = x_train_real
+        x_test = x_test_real
+
+    type_str = "norm" if transform else "no_norm"
+
+    if model == 0:
+        regr = linear_model.LinearRegression()
+        regr.fit(x_train.iloc[:, [5, 7]], y_train)
+
+        y_pred = regr.predict(x_test.iloc[:, [5, 7]])
+        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+        rsquared = r2_score(y_test, y_pred)
+
+        plt.figure()
+        plt.scatter(y_test, y_pred)
+        plt.xlabel("True rings")
+        plt.ylabel("Predicted rings")
+        plt.title(f"Linear: y_true vs y_pred ({type_str}) Selected Features")
+        plt.savefig(f"linreg_scatter_{type_str}_selected_Features.png")
+        plt.close()
+
+        # residuals histogram
+        res = np.asarray(y_test) - np.asarray(y_pred)
+        plt.figure()
+        plt.hist(res, bins=30)
+        plt.xlabel("Residual = y_true - y_pred"); plt.ylabel("count")
+        plt.title(f"Linear: residuals hist ({type_str}) Selected Features")
+        plt.savefig(f"regression resid hist {type_str}_selected_features.png");
+        plt.close()
+
+        # residuals vs fitted
+        plt.figure()
+        plt.scatter(y_pred, res)
+        plt.xlabel("Fitted")
+        plt.ylabel("Residual")
+        plt.title(f"Linear: residuals vs fitted ({type_str}) Selected Features")
+        plt.savefig(f"regression resid vs fit {type_str}_selected_features.png")
+        plt.close()
+
+        return rmseselec, rsquaredselec
+
+    elif model == 1:
+        y_train_binary = (np.asarray(y_train) >= 7)
+        y_test_binary = (np.asarray(y_test) >= 7) # note this is y true
+        regr = linear_model.LogisticRegression(max_iter=1000)
+        regr.fit(x_train.iloc[:, [5, 7]], y_train_binary)
+
+        y_probs = regr.predict_proba(x_test.iloc[:, [5, 7]])[:, 1]
+        y_pred = (y_probs >= 0.5)
+        acc = accuracy_score(y_test_binary, y_pred)
+        auc = roc_auc_score(y_test_binary, y_probs)
+
+        fpr, tpr, thresh = roc_curve(y_test_binary, y_probs)
+        plt.figure()
+        plt.plot(fpr, tpr); plt.plot([0,1], [0,1], linestyle="--")
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")
+        plt.title(f"ROC curve ({type_str}) Selected Features")
+        plt.tight_layout()
+        plt.savefig(f"logistic roc {type_str} selected features.png")
+        plt.close()
+
+        return accselec, aucselec
+
+def neural(x_train, x_test, y_train, y_test): #refer to trial.py
+    transformer = MinMaxScaler().fit(x_train.iloc[:,1:])
     x_train.iloc[:, 1:] = transformer.transform(x_train.iloc[:, 1:])
     x_test.iloc[:, 1:] = transformer.transform(x_test.iloc[:, 1:])
   
@@ -148,7 +220,6 @@ def neural(x_train, x_test, y_train, y_test): #compare with best approach from p
   
 def process_data(df):
     df.iloc[:, 0] = df.iloc[:, 0].replace({"M": 0, "F": 1, "I": 2})
-
 
 def main():
     df = pd.read_csv('abalone.data', header=None, names=["sex", "length", "diameter", "height", "whole_weight", "shucked_weight", "viscera_weight", "shell_weight", "rings"])
@@ -165,7 +236,13 @@ def main():
     rsquared_norm = np.zeros(NUM_EXPERIMENTS)
     acc_norm = np.zeros(NUM_EXPERIMENTS)
     auc_norm = np.zeros(NUM_EXPERIMENTS)
+    
+    rmse_sel = np.zeros(NUM_EXPERIMENTS)
+    rsquared_sel = np.zeros(NUM_EXPERIMENTS)
 
+    rmse_nn = np.zeros(NUM_EXPERIMENTS)
+    rsquared_nn = np.zeros(NUM_EXPERIMENTS)
+    
     data_inputx = df.iloc[:, 0:-1]
     data_inputy = df.iloc[:, -1]
     print(data_inputx)
@@ -181,24 +258,37 @@ def main():
         acc_val, auc_val = scipy_linear_mod(x_train, x_test, y_train, y_test, True, 1)
         rmse_norm[i], rsquared_norm[i], acc_norm[i], auc_norm[i] = rmse_val, rsquared_val, acc_val, auc_val
     
-    print(f"RMSE no norm Mean: {rmse.mean()}, with norm: {rmse_norm.mean()}")
-    print(f"RMSE no norm SD: {rmse.sd()}, with norm: {rmse_norm.sd()}")
+        rmseselec, rsquaredselec = selected(x_train, x_test, y_train, y_test, True, 0)
+        accselec, aucselec = selected(x_train, x_test, y_train, y_test, True, 1)
+        rmse_sel[i], rsquared_sel[i], acc_sel[i], auc_sel[i] = rmseselec, rsquaredselec, accselec, aucselec
+    
+        rmse_val, rsquared_val = neural(x_train, x_test, y_train, y_test)
+        rmse_nn[i], rsquared_nn[i] = rmse_val, rsquared_val
 
+    print(f"RMSE no norm Mean: {rmse.mean()}, with norm: {rmse_norm.mean()}")
+    print(f"RMSE no norm SD: {rmse.std()}, with norm: {rmse_norm.std()}")
     print(f"Rsquared no norm Mean: {rsquared.mean()}, with norm: {rsquared_norm.mean()}")
-    print(f"Rsquared no norm SD: {rsquared.sd()}, with norm: {rsquared_norm.sd()}")
+    print(f"Rsquared no norm SD: {rsquared.std()}, with norm: {rsquared_norm.std()}")
 
     print(f"Accuracy no norm Mean: {acc.mean()}, with norm: {acc_norm.mean()}")
-    print(f"Accuracy no norm SD: {acc.sd()} with norm: {acc_norm.sd()}")
-
+    print(f"Accuracy no norm SD: {acc.std()} with norm: {acc_norm.std()}")
     print(f"AUC no norm Mean: {auc.mean()}, with norm: {auc_norm.mean()}")
-    print(f"AUC no norm SD: {auc.sd()} with norm: {auc_norm.sd()}")
+    print(f"AUC no norm SD: {auc.std()} with norm: {auc_norm.std()}")
+    
+    print(f"RMSE of Selected Features Mean: {rmse_sel.mean()}")
+    print(f"RMSE of Selected Features SD: {rmse_sel.std()}")
+    print(f"Rsquared of Selected Features Mean: {rsquared_sel.mean()}")
+    print(f"Rsquared of Selected Features SD: {rsquared_sel.std()}")
+
+    print(f"Accuracy of Selected Features Mean: {acc_sel.mean()}")
+    print(f"Accuracy of Selected Features SD: {acc_sel.std()}")
+    print(f"AUC of Selected Features Mean: {auc_sel.mean()}")
+    print(f"AUC of Selected Features SD: {auc_sel.std()}")
     
     print(f"RMSE of Neural Network Mean: {rmse_nn.mean()}")
-    print(f"RMSE of Neural Network SD: {rmse_nn.sd()}")
-    
+    print(f"RMSE of Neural Network SD: {rmse_nn.std()}")
     print(f"Rsquared of Neural Network Mean: {rsquared_nn.mean()}")
-    print(f"Rsquared of Neural Network SD: {rsquared_nn.sd()}")
+    print(f"Rsquared of Neural Network SD: {rsquared_nn.std()}")
 
-#Ask about reporting for training set
 if __name__ =='__main__':
     main() 
